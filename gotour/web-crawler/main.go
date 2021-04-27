a06d576249a142f6fe9d980e0fed5b183ef410fe
Bruno Kawka <brunokawka@gmail.com>
2021-04-27 18:22:04 +0200

    Refactor web-crawler example with sync.WaitGroup sync and channels

--- gotour/web-crawler/main.go
+++ gotour/web-crawler/main.go
@@ -9,18 +9,33 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
+// -- Fetcher --
 type Fetcher interface {
 	// Fetch returns the body of URL and
 	// a slice of URLs found on that page.
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+// -- CrawlResult --
+type CrawlResult struct {
+	url  string
+	body string
+	err  error
+}
+
+func (cr *CrawlResult) String() string {
+	if cr.err != nil {
+		return fmt.Sprint("error: ", cr.url, " message: ", cr.err)
+	}
+	return fmt.Sprint("found: ", cr.url, " - ", cr.body)
+}
+
+// -- SafeCache --
 type SafeCache struct {
 	urls map[string]struct{}
-	mux sync.Mutex
+	mux  sync.Mutex
 }
 
 func (sc *SafeCache) Has(url string) bool {
@@ -36,35 +51,60 @@ func (sc *SafeCache) Push(url string) {
 	sc.urls[url] = struct{}{}
 }
 
+// -- Crawler --
 type Crawler struct {
-	cache *SafeCache
+	cache   *SafeCache
 	fetcher Fetcher
 }
 
-func (crl *Crawler) Crawl (url string, depth int) {
+func (crl *Crawler) Crawl(url string, depth int, cr chan *CrawlResult) {
+	defer close(cr)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	crl.crawlRecursively(url, depth, cr, &wg)
+	wg.Wait()
+}
+
+func (crl *Crawler) crawlRecursively(url string, depth int, cr chan *CrawlResult, wg *sync.WaitGroup) {
+	defer wg.Done()
 	if depth <= 0 || crl.cache.Has(url) {
 		return
 	}
 	crl.cache.Push(url)
 	body, urls, err := crl.fetcher.Fetch(url)
+	cr <- &CrawlResult{url: url, err: err, body: body}
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
-	fmt.Printf("found: %s %q\n", url, body)
 	for _, u := range urls {
-		go crl.Crawl(u, depth-1)
+		wg.Add(1)
+		go crl.crawlRecursively(u, depth-1, cr, wg)
+	}
+}
+
+// -- Main --
+
+func printCrawlResults(cr chan *CrawlResult) {
+	for {
+		v, ok := <-cr
+		if !ok {
+			break
+		}
+		fmt.Println(v)
 	}
 }
 
 func main() {
 	crl := Crawler{
-		cache: &SafeCache{urls: make(map[string]struct{})}, 
+		cache:   &SafeCache{urls: make(map[string]struct{})},
 		fetcher: fetcher,
 	}
-	go crl.Crawl("https://golang.org/", 4)
-	time.Sleep(time.Second)
+	cr := make(chan *CrawlResult)
+	go crl.Crawl("https://golang.org/", 4, cr)
+	printCrawlResults(cr)
 }
+
+// -- Fake Fetcher Implementation --
 
 // fakeFetcher is Fetcher that returns canned results.
 type fakeFetcher map[string]*fakeResult
